30dbcf02b59e9e51f3060af8f6b604e5f894172e
etg-git <etg9307@naver.com>
2020-04-06 16:17:05 +0900

    project_01

--- projectCollection/vendingMachine/VendingMachine.java
+++ projectCollection/vendingMachine/VendingMachine.java
@@ -3,25 +3,25 @@
 
 import java.util.Scanner;
 class VendingMachine {
-	private String color; // ����
-	private int balance;
-	private String manufacture; // ������
-
-	private Money money; //����
-	private Money coin; //����
-
+	private String color; // 색상
+	private int balance; 
+	private String manufacture; // 제조사
+	
+	private Money money; //지폐
+	private Money coin; //동전
+	
 	private Beverage coke;
 	private Beverage pepsi;
 	private Beverage powerAde;
 	private Beverage sprite;
 	private Beverage cider;
-
-	static final int d_coke = 1;
-	static final int d_pepsi = 2;
-	static final int d_powerAde = 3;
-	static final int d_sprite = 4;
-	static final int d_cider = 5;
-
+	
+	public static final int D_COKE = 1;
+	public static final int D_PEPSI = 2;
+	public static final int D_POWERADE = 3;
+	public static final int D_SPRITE = 4;
+	public static final int D_CIDER = 5;
+	
 	public VendingMachine(String color, String manufacture, Beverage coke, Beverage pepsi, Beverage powerAde, Beverage sprite, Beverage cider, Money money, Money coin) {
 		setColor(color);
 		setManufacture(manufacture);
@@ -95,67 +95,70 @@ class VendingMachine {
 	}
 	public void inputMoneySum() {
 		int sum = money.moneySum() + coin.coinSum();
-		balance += sum;
+		balance += sum;	
 	}
+
+	//음료수 반환 메소드, 다른번호 눌렸을때 다시 선택
 	public Beverage choicePick() {
 
-			Scanner sc = new Scanner(System.in);
 			int drink;
-
+			Scanner sc = new Scanner(System.in);
 			String info = "=====================================================\n";
 				info += "||         	                                    ||\n";
-				info +=	"||  1.��ī�ݶ�         2. ����         3.�Ŀ����̵� ||\n";
-				info +=	"||    800��(" +soldOut(coke.getCount()) + ")    800��(" +soldOut(pepsi.getCount()) + ")      1000��(" +soldOut(powerAde.getCount()) + ") ||\n";
-				info +=	"||  4.��������Ʈ            5.ĥ�����̴�            ||\n";
-				info +=	"||    1500��("+soldOut(sprite.getCount())+")          1200��(" +soldOut(cider.getCount()) + ")  	    ||\n";
+				info +=	"||  1.코카콜라         2. 펩시         3.파워에이드 ||\n";
+				info +=	"||    800원(" +soldOut(coke.getCount()) + ")    800원(" +soldOut(pepsi.getCount()) + ")      1000원(" +soldOut(powerAde.getCount()) + ") ||\n";
+				info +=	"||  4.스프라이트            5.칠성사이다            ||\n";
+				info +=	"||    1500원("+soldOut(sprite.getCount())+")          1200원(" +soldOut(cider.getCount()) + ")  	    ||\n";
 				info += "||         	                                    ||\n";
-				info += "||                  0 : �ܵ���ȯ                    ||\n";
 				info +=	"=====================================================\n";
 
 				System.out.println(info);
-				System.out.print("�������� �����ϼ��� : ");
+				System.out.print("음료수를 선택하세요 : ");
 				drink = sc.nextInt();
 
-			if(d_coke == drink && balance >= 800 && soldOut(coke.getCount())) {
-				 return result(getCoke(), balance-=800, drink);
+			if(D_COKE == drink && balance >= 800 && soldOut(coke.getCount())) {
+				 return returnDrink(getCoke(), balance-=800, drink);
 			}
-			else if(d_pepsi == drink && balance >= 800 && soldOut(pepsi.getCount())) {
-				 return result(getPepsi(), balance-=800, drink);
+			else if(D_PEPSI == drink && balance >= 800 && soldOut(pepsi.getCount())) {
+				 return returnDrink(getPepsi(), balance-=800, drink);
 			 }
-			 else if(d_powerAde == drink &&  balance >= 1000  && soldOut(powerAde.getCount())) {
-				 return result(getPowerAde(), balance-=1000, drink);
+			 else if(D_POWERADE == drink &&  balance >= 1000  && soldOut(powerAde.getCount())) {
+				 return returnDrink(getPowerAde(), balance-=1000, drink);
 			 }
-			 else if(d_sprite == drink && balance >= 1500  && soldOut(sprite.getCount())) {
-				 return result(getSprite(), balance -= 1500,  drink);
+			 else if(D_SPRITE == drink && balance >= 1500  && soldOut(sprite.getCount())) {
+				 return returnDrink(getSprite(), balance -= 1500,  drink);
 			 }
-			 else if(d_cider == drink &&  balance >= 1200  && soldOut(cider.getCount())) {
-				 return result(getCider(), balance-=1200,  drink);
-			 }
-			 else if(drink == 0) {
-				 return null;
+			 else if(D_CIDER == drink &&  balance >= 1200  && soldOut(cider.getCount())) {
+				 return returnDrink(getCider(), balance-=1200,  drink);
 			 }
 			 else {
-				System.out.println("�ٽû�������");
-				return choicePick();
+				 System.out.println("다시 뽑으세요");
+				 return choicePick();
 			 }
 	}
-	private Beverage result(Beverage beverage,int balance, int drink) {
-			DrinkDecrease(drink);
-			Scanner sc = new Scanner(System.in);
-			System.out.print("���̱� : 1, �������ʱ� : �ƹ���ȣ  ====>  ");
-			int select = sc.nextInt();
-
-			if(select == 1) {
-				System.out.println(beverage);
-				return choicePick();
-			}
-			else return beverage;
+	//pick메소드 중복되는 구문이 많아 간략하게 함 
+	private Beverage returnDrink(Beverage beverage, int balance, int drink) {
+			drinkDecrease(drink);
+			System.out.println(beverage);
+			return new Beverage(beverage.getName(), beverage.getAmount(), 1);
 	}
+	//잔액반환
 	public int returnMoney() {
-		System.out.println(getBalance() + "�� ��ȯ�մϴ�");
+		Scanner sc = new Scanner(System.in);
+		System.out.print(getBalance() + "원 남음 더 뽑으실? : 1, 잔돈반환 : 아무번호 ====> ");
+		int choice = sc.nextInt();
+
+		if(choice == 1) {
+			choicePick();
+			returnMoney();
+			return getBalance();
+		}
+		System.out.println(getBalance() + "원 나왔습니다");
 		return getBalance();
 	}
-	private void DrinkDecrease(int drink) {
+
+	//갯수감소
+	private void drinkDecrease(int drink) {
 		if(drink == 1) {
 			coke.setCount(coke.getCount()-1);
 		}
@@ -172,16 +175,16 @@ class VendingMachine {
 			cider.setCount(cider.getCount()-1);
 		}
 	}
-	private boolean soldOut(int number) { // ������ ������ true ������ false
-		if(number <= 0) { //������ 0�϶� false
+	private boolean soldOut(int number) { // 음료수 있으면 true 없으면 false
+		if(number <= 0) { //갯수가 0일때 false
 				return false;
-		} else { //������ ���������� true
+		} else { //갯수가 아직있을때 true
 			return true;
 		  }
 	}
-	public String toString() {
-		String info = balance + "�� �ܾ�";
-
+	public String toString() {	
+		String info = balance + "원 잔액";
+		
 		return info;
 	}
 }
